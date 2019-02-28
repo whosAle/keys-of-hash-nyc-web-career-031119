e8adf8e93aa242aafeceee1dc21e302c9a6c1cfe
A Belgrave <abelgrave95@gmail.com>
2019-02-28 18:29:53 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,7 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     result = []
-    self.select { |k,v| result << k if v == arguments }
+    arguments.each { |arg| self.select { |k,v| result << k if v == arguments } }
     result
   end
 end
